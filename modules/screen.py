a765d38cd77b72d515bf578122fe73b967f6aefb
Lord-Chaos-YT <thumper540@gmail.com>
2022-04-28 16:46:07 -0600

    Screen stuff:

--- modules/screen.py
+++ modules/screen.py
@@ -1,13 +1,37 @@
-from ast import MatchOr
-
+from math import floor
+from networking import networking
 
 class TileMap:
-    def __init__(self, width, height) -> None:
-        self.matrix = [[0] * width] * height
-        temp: list = []
-        for i in range(0, height):
-            temp.insert(i, 0)
-        for i in range(0, width):
-            self.matrix.insert(i, temp.copy())
+    tileSize: float
+    matrix: list
+    network: networking
+    def __init__(self, initNetwork, initTileSize) -> None:
+        self.tileSize = initTileSize
+        self.network = initNetwork
+        self.matrix = self.network.receivemap().tolist()
         return
-    matrix: list = []
+    
+    def checkCollision(self, rect: tuple) -> bool:
+        offset = (
+            rect[0] % self.tileSize,
+            rect[1] % self.tileSize,
+            (rect[0] + rect[2]) % self.tileSize,
+            (rect[1] + rect[3]) % self.tileSize
+        )
+
+        tileIdx = (
+            (
+                floor((rect[0] - offset[0]) / self.tileSize),
+                floor((rect[1] - offset[1]) / self.tileSize)
+            ),
+            (
+                floor(((rect[0] + rect[2]) - offset[0]) / self.tileSize),
+                floor(((rect[1] + rect[3]) - offset[0]) / self.tileSize)
+            )
+        )
+
+        for idx in tileIdx:
+            x, y = idx
+            if self.matrix[x][y]:
+                return True
+        return False
