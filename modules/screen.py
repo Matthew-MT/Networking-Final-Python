a4d643234c05074019d41a6fae65e58746c13f5b
Lord-Chaos-YT <thumper540@gmail.com>
2022-05-04 08:37:41 -0600

    Screen methods

--- modules/screen.py
+++ modules/screen.py
@@ -1,5 +1,8 @@
 from math import floor
+
+from numpy import tile
 from networking import networking
+from tkinter import Canvas
 
 class TileMap:
     tileSize: float
@@ -25,14 +28,62 @@ class TileMap:
                 floor((rect[1] - offset[1]) / self.tileSize)
             ),
             (
-                floor(((rect[0] + rect[2]) - offset[0]) / self.tileSize),
-                floor(((rect[1] + rect[3]) - offset[0]) / self.tileSize)
+                floor(((rect[0] + rect[2]) - offset[2]) / self.tileSize),
+                floor(((rect[1] + rect[3]) - offset[3]) / self.tileSize)
             )
         )
 
-        if self.matrix[tileIdx[0][0]][tileIdx[0][1]]\
+        if tileIdx[0][0] < 0 or tileIdx[0][1] < 0\
+        or tileIdx[1][0] > len(self.matrix)\
+        or tileIdx[1][1] > len(self.matrix[0])\
+        or self.matrix[tileIdx[0][0]][tileIdx[0][1]]\
         or self.matrix[tileIdx[0][0]][tileIdx[1][1]]\
         or self.matrix[tileIdx[1][0]][tileIdx[0][1]]\
         or self.matrix[tileIdx[1][0]][tileIdx[1][1]]:
             return True
         return False
+    
+    def getDrawScreen(self, rect: tuple):
+        offset = (
+            rect[0] % self.tileSize,
+            rect[1] % self.tileSize,
+            (rect[0] + rect[2]) % self.tileSize,
+            (rect[1] + rect[3]) % self.tileSize
+        )
+
+        tileBounds = (
+            (
+                floor((rect[0] - offset[0]) / self.tileSize),
+                floor((rect[1] - offset[1]) / self.tileSize)
+            ),
+            (
+                floor((rect[2] - offset[2]) / self.tileSize),
+                floor((rect[3] - offset[3]) / self.tileSize)
+            )
+        )
+
+        if tileBounds[0][0] < 0:
+            tileBounds[0][0] = 0
+        if tileBounds[0][1] < 0:
+            tileBounds[0][1] = 0
+        if tileBounds[1][0] >= len(self.matrix):
+            tileBounds[1][0] = len(self.matrix) - 1
+        if tileBounds[1][1] >= len(self.matrix[0]):
+            tileBounds[1][1] = len(self.matrix[0]) - 1
+
+        drawMatrix: list = []
+
+        for x in range(tileBounds[0][0], tileBounds[1][0]):
+            drawMatrix.append([])
+            for y in range(tileBounds[0][1], tileBounds[1][1]):
+                fill: str = ""
+                if self.matrix[x][y] == 1:
+                    fill = "black"
+                elif self.matrix[x][y] == 0:
+                    fill = "white"
+                drawMatrix.append(tuple(
+                    (x * self.tileSize) - offset[0],
+                    (y * self.tileSize) - offset[1],
+                    (x * (self.tileSize + 1)) - offset[0],
+                    (y * (self.tileSize + 1)) - offset[1]))
+        return drawMatrix
